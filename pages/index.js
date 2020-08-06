f2c288122b950b00c05d8be0f9db88680d8063f6
Shine Sudarsanan <shineklbm@gmail.com>
2020-08-07 01:51:11 +0530

    mega menu work in progress

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,49 @@
+import { useState } from 'react';
 import Head from 'next/head';
-import { Container, Row, Col, Button } from '../components/vendor/reactstrap';
+import {
+  Container,
+  Row,
+  Col,
+  Nav,
+  NavbarBrand,
+  NavItem,
+  NavLink,
+  NavbarToggler,
+  Collapse,
+} from '../components/vendor/reactstrap';
 
 export default function Home() {
   // if the following return has more than tags then wrap them in ()
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
   return (
-    <Container>
-      <Row>
-        <Col>
-          <h1>Defualt Home Page</h1>
-          <Button color="danger">Hello</Button>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <nav className="navbar navbar-expanded-lg navbar-dark bg-info">
+        <Container fluid={true}>
+          {/* Brand Logo Text */}
+          <NavbarBrand href="/" className="mr-auto">
+            Mega Menu
+          </NavbarBrand>
+          {/* Brand Logo Text Ends */}
+          {/* Responsive Menu */}
+          <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+          {/* Responsive Menu Ends */}
+        </Container>
+      </nav>
+    </>
   );
 }
