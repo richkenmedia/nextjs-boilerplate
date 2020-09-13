b874f5e6e04018e9128457cc983596111017c11a
Shine Sudarsanan <shineklbm@gmail.com>
2020-09-14 00:39:06 +0530

    updated the pages

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,14 @@ import { Container, Row, Col, Button } from '../components/vendor/reactstrap';
 export default function Home() {
   // if the following return has more than tags then wrap them in ()
   return (
-    <Container fluid={true}>
-      <Row>
-        <Col>
-          <h1>Home Page</h1>
-        </Col>
-      </Row>
-    </Container>
+    <div className="bg-danger vh-100">
+      <Container fluid={true}>
+        <Row>
+          <Col className="pt-5">
+            <h1 className="text-center text-white">Home Page</h1>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
